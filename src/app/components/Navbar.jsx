0f89297b9dd3c0c9538ab569ef398ebbb0d4dad6
Baskaran Anuruththan <85021506+anuruththan@users.noreply.github.com>
2025-10-20 23:25:12 +0530

    Update Navbar.jsx

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
           href={"/"}
           className="text-2xl md:text-5xl text-white font-semibold"
         >
-          <Image src="/anuru_logo_final.png" alt="Logo"
+          <Image src={`${process.env.NEXT_PUBLIC_BASE_PATH}/anuru_logo_final.png`} alt="Logo"
                  width={100}
                  height={100}
                  className="h-10 md:h-16 w-auto object-contain" />
